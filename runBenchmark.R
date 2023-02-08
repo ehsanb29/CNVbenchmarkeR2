d35d6af255410f7bd4d3572530281693a8e56c05
j <jpuntomarcos@gmail.com>
2023-02-08 17:32:46 +0200

    Bug fixed

--- runBenchmark.R
+++ runBenchmark.R
@@ -34,47 +34,48 @@ dir.create("output", showWarnings = F)
 
 # Panelcn.mops
 if (tools$panelcn == T){
-  cat(as.character(Sys.time()), " - Executing panelcn.MOPS")
+  cat(as.character(Sys.time()), " - Executing panelcn.MOPS\n")
   cmd <- paste("Rscript tools/panelcnmops/runPanelcnmops.r tools/panelcnmops/panelcnmopsParams.yaml", args$datasets, " > logs/panelcnmops.log 2>&1")
   system(cmd)
 }
 
 # Decon
 if (tools$decon == T){
-  cat(as.character(Sys.time()), " - Executing DECoN")
+  cat(as.character(Sys.time()), " - Executing DECoN\n")
   cmd <- paste("Rscript tools/decon/runDecon.r tools/decon/deconParams.yaml", args$datasets, " > logs/decon.log 2>&1")
   system(cmd)
 }
 
 # ExomeDepth
 if (tools$panelcn == T){
-  cat(as.character(Sys.time()), " - Executing ExomeDepth")
+  cat(as.character(Sys.time()), " - Executing ExomeDepth\n")
   cmd <- paste("Rscript tools/exomedepth/runExomedepth.r tools/exomedepth/exomedepthParams.yaml", args$datasets, " > logs/exomedepth.log 2>&1")
   system(cmd)
 }
 
 # CODEX2
 if (tools$panelcn == T){
-  cat(as.character(Sys.time()), " - Executing CODEX2")
+  cat(as.character(Sys.time()), " - Executing CODEX2\n")
   cmd <- paste("Rscript tools/codex2/runCodex2.r tools/codex2/codex2Params.yaml", args$datasets, " > logs/codex2.log 2>&1")
   system(cmd)
 }
 
 # ClinCNV
 if (tools$clincnv == T){
-  cat(as.character(Sys.time()), " - Executing ClinCNV")
-  cmd <- paste("Rscript tools/clincnv/runClincnv.r tools/clincnv/clincnvParams.yaml", args$datasets, , " > logs/clincnv.log 2>&1")
+  cat(as.character(Sys.time()), " - Executing ClinCNV\n")
+  cmd <- paste("Rscript tools/clincnv/runClincnv.R tools/clincnv/clincnvParams.yaml", args$datasets, " > logs/clincnv.log 2>&1")
   system(cmd)
 }
 
 # CoNVaDING
 if (tools$clincnv == T){
-  cat(as.character(Sys.time()), " - Executing CoNVaDING")
+  cat(as.character(Sys.time()), " - Executing CoNVaDING\n")
   cmd <- paste("Rscript tools/convading/runConvading.r tools/convading/convadingParams.yaml", args$datasets, " > logs/convading.log 2>&1")
   system(cmd)
 }
 
 ##  Generate summary file  ##
-cat(as.character(Sys.time()), " -  Generating summary file")
+
+cat(as.character(Sys.time()), " - Generating summary file")
 cmd <- paste("Rscript utils/summary.r", args$tools, args$datasets, "> logs/summary.log 2>&1")
 system(cmd)
