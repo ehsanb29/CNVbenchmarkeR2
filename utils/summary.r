c3399dd194722aba288853b6d0e0061d27f29a73
emunte <emunte@idibell.cat>
2023-02-28 11:06:51 +0100

    Bug fixed

--- utils/summary.r
+++ utils/summary.r
@@ -3,22 +3,23 @@
 source(if (basename(getwd()) == "utils") "cnvStats.r" else "utils/cnvStats.r") # Load class definitions
 suppressPackageStartupMessages(library(yaml))
 
-# load algorithms and datasets from yaml files
+# load tools and datasets from yaml files
 args <- commandArgs(TRUE)
 cat("\n"); print(args)
 if(length(args)>0) {
-  algorithmsParamsFile <- args[1]
+  toolsParamsFile <- args[1]
   datasetsParamsFile <- args[2]
 } else {
-  algorithmsParamsFile <- "algorithms.yaml"
+  toolsParamsFile <- "tools.yaml"
   datasetsParamsFile <- "datasets.yaml"
 }
 
+
 #Load the parameters file
-algorithms <- yaml.load_file(algorithmsParamsFile)$algorithms
+tools <- yaml.load_file(toolsParamsFile)
 datasets <- yaml.load_file(datasetsParamsFile)
 
-# Run summary for all datasets and algorithms
+# Run summary for all datasets and tools
 for (dName in names(datasets)) {
   dataset <- datasets[[dName]]
 
@@ -28,11 +29,11 @@ for (dName in names(datasets)) {
     ss <- SummaryStats()
     ss$loadValidatedResults(path = dataset$validated_results_file,
                             datasetName = dName,
-                            bedFile = dataset$bed_file)  
+                            bedFile = dataset$bed_file)
 
     # Calculate metrics
-    for (alg in names(algorithms)) {
-      useAlg <- algorithms[[alg]]
+    for (alg in names(tools)) {
+      useAlg <- tools[[alg]]
       if (useAlg){
         outputFolder <- file.path(getwd(), "output", paste0(alg, "-", dName))
         ss$loadAlgorithmResults(outputFolder = outputFolder,
@@ -41,9 +42,9 @@ for (dName in names(datasets)) {
                                 bedFile = dataset$bed_file)
       }
     }
-    
+
     # Write results to summary file
     ss$writeSummary(file.path(getwd(), "output", "summary", paste0("summary-", dName, ".txt")), dName)
     ss$writeCSVresults(file.path(getwd(), "output", "summary", paste0("results-", dName, ".csv")), dName)
   }
-}  
+}
