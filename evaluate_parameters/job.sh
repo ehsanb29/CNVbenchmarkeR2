0aa2c7bde936aa94ee1f8c0d5df771a22e87ef2a
Elisabet Munte <emunte@bioe01.biocluster.idibell.cat>
2024-02-08 16:08:43 +0100

    update mem

--- evaluate_parameters/job.sh
+++ evaluate_parameters/job.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-#SBATCH --time=4000
-#SBATCH --mem=3GB
+#SBATCH --time=3000
+#SBATCH --mem 48000M
+#SBARCH --mem-per-cpu 48000M
 #SBATCH --nodes=1
-#SBATCH --job-name="TEST"
-#SBATCH --cpus-per-task=1
+#SBATCH --job-name="TEST48gb"
+#SBATCH --cpus-per-task=8
+#SBATCH --mail-user emunte@idibell.cat
+#SBATCH --mail-type BEGIN        # send email when job begins
+#SBATCH --mail-type END          # send email when job ends
+#SBATCH --mail-type FAIL         # send email if job fails
+
 
 
 echo "Run at:"
@@ -16,8 +22,7 @@ evaluate_par=$5
 log=$6
 
 #echo Rscript $script $params $dataset $include_dir > $log 2>&1
-#srun --mem=24G -c 4 
-srun --mem=3GB -c 1 Rscript $script $params $dataset $include_dir $evaluate_par > $log 2>&1
+srun --mem 48000M -c 8 Rscript $script $params $dataset $include_dir $evaluate_par > $log 2>&1
 
 
 
