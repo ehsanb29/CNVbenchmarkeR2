c00d67ad15a23f46179eafcabd963a4c0bf38781
Elisabet Munte <emunte@bioe01.biocluster.idibell.cat>
2024-04-09 15:46:19 +0200

    revision

--- tools/cnvkit/runCnvkit.r
+++ tools/cnvkit/runCnvkit.r
@@ -1,12 +1,12 @@
 # Runs cnvkit over the datasets cofigured at [datasets_params_file]
-#USAGE: Rscript runCnvkit.R [clearCNV_params_file] [datasets_params_file] [include_temp_files]
+#USAGE: Rscript runCnvkit.R [cnvkit_params_file] [datasets_params_file] [include_temp_files]
 print(paste("Starting at", startTime <- Sys.time()))
 suppressPackageStartupMessages(library(yaml))
 source(if (basename(getwd()) == "optimizers") "../utils/utils.r" else "utils/utils.r") # Load utils functions
 library(dplyr)
 
-
-# Read args
+#Get parameters----
+## Read args ----
 args <- commandArgs(TRUE)
 if(length(args)>0) {
   cnvkitParamsFile <- args[1]
@@ -18,15 +18,18 @@ if(length(args)>0) {
   includeTempFiles <- "true"
 }
 
-#Load the parameters file
+##Load the parameters files----
 params <- yaml.load_file(cnvkitParamsFile)
 datasets <- yaml.load_file(datasetsParamsFile)
+
+# print params
 print(paste("Params for this execution:", list(params)))
+print(paste("Datasets for this execution:", list(datasets)))
 
-
-#get cnvkit folder
+##Get cnvkit folder----
 cnvkitFolder <- file.path(params$cnvkitFolder)
 
+# Dataset iteration ----
 # go over datasets and run cnvkit for those which are active
 for (name in names(datasets)) {
   dataset <- datasets[[name]]
@@ -51,8 +54,9 @@ for (name in names(datasets)) {
 
     # Get bam files
     bamFiles <- list.files(bamsDir, pattern = '*.bam$', full.names = TRUE)
-
-    # Access: calculate accessible coordinates of the chromosome sequences
+    ## Run cnvkit----
+    ### Access----
+    #calculate accessible coordinates of the chromosome sequences
     cmd <- paste(".",
                  cnvkitFolder, "\n",
                  "cnvkit.py",  "access",
@@ -62,7 +66,8 @@ for (name in names(datasets)) {
 
     print(cmd);system(cmd)
 
-    #Autobin: Quickly estimate read counts or depths in a BAM file to estimate reasonable on- and (if relevant) off-target bin sizes.
+    ###Autobin---- 
+    #Quickly estimate read counts or depths in a BAM file to estimate reasonable on- and (if relevant) off-target bin sizes.
     cmd <- paste(".",
                  cnvkitFolder, "\n",
                  "cnvkit.py", "autobin",
@@ -82,6 +87,7 @@ for (name in names(datasets)) {
     #create a folder to store calls files
     dir.create(file.path(outputFolder, "calls"))
 
+    
     for(i in seq_len(length(bamFiles))){
       #For each sample
       sampleName <- basename(tools::file_path_sans_ext(bamFiles))
@@ -105,8 +111,8 @@ for (name in names(datasets)) {
                    file.path(outputFolder, "ROIs-ICR96-panelcnDataset.antitarget.bed"),
                    "-o", file.path(outputFolder, paste0(sampleName[i], ".antitargetcoverage.cnn" )))
       print(cmd); system(cmd)
-    }
-
+      }
+    ### Coverage of target and antitarget----
     for(i in seq_len(length(bamFiles))){
       #Build a reference for each sample, including all samples except the sample of interest
       #Get cnn Target and antitarget files
@@ -123,7 +129,8 @@ for (name in names(datasets)) {
 
 
 
-      #Fix: uncorrected target and antitarget coverage tables (.cnn) and correct for biases in regional coverage and GC content, according to the given reference.
+      ###Fix----
+      #uncorrected target and antitarget coverage tables (.cnn) and correct for biases in regional coverage and GC content, according to the given reference.
       cmd <- paste(".",
                    cnvkitFolder, "\n",
                    "cnvkit.py", "fix",
@@ -134,7 +141,8 @@ for (name in names(datasets)) {
      paste(cmd); system(cmd)
 
 
-      #Segment infer discrete copy number segments from the given coverage table:
+      ###Segment----
+      #infer discrete copy number segments from the given coverage table:
       cmd <- paste(".",
                    cnvkitFolder, "\n",
                    "cnvkit.py", "segment",
@@ -145,7 +153,8 @@ for (name in names(datasets)) {
                    "-o", file.path(outputFolder, paste0(sampleName[i], ".cns")))
       paste(cmd);system(cmd)
 
-      #Segmetrics: Calculate summary statistics of the residual bin-level log2 ratio estimates from the segment means, similar to the existing metrics command, but for each segment individually.
+      ###Segmetrics----
+      #Calculate summary statistics of the residual bin-level log2 ratio estimates from the segment means, similar to the existing metrics command, but for each segment individually.
       cmd <- paste(".",
                    cnvkitFolder, "\n",
                    "cnvkit.py", "segmetrics",
@@ -158,7 +167,7 @@ for (name in names(datasets)) {
 
       paste(cmd);system(cmd)
 
-      #call cnvs
+      ###Call cnvs----
       cmd <- paste(".",
                    cnvkitFolder, "\n",
                    "cnvkit.py", "call",
@@ -169,7 +178,7 @@ for (name in names(datasets)) {
                    "-o", file.path(outputFolder, paste0("calls/", sampleName[i], ".call.cns")))
       paste(cmd);system(cmd)
 
-      #bintest
+      ###Bintest----
       cmd <- paste(".",
                    cnvkitFolder, "\n",
                    "cnvkit.py", "bintest",
@@ -190,80 +199,84 @@ for (name in names(datasets)) {
                    "-o", file.path(outputFolder, paste0("calls/", sampleName[i], "bintest.call.cns")))
 
       print(cmd); system(cmd)
+      }
 
 
-    }
+    #Save results----
+    ##TSV file----
+    #Put all results in the same file
+    cnvData  <- data.frame()
+    for(s in sampleName){
+      #read the call files and merge them
+      callFiles <- read.table(file.path(outputFolder, paste0("calls/", s, ".call.cns")), header = TRUE) %>% 
+        dplyr::mutate(p_bintest = NA) %>% 
+        dplyr::relocate(p_bintest, .after = depth)
+      callBinFiles <- read.table(file.path(outputFolder, paste0("calls/", s, "bintest.call.cns")), header = TRUE)
+      sam <- rbind(callFiles, callBinFiles)
 
-
-
-#Put all results in the same file
-cnvData  <- data.frame()
-
-for(s in sampleName){
-  #read the call files and merge them
-  callFiles <- read.table(file.path(outputFolder, paste0("calls/", s, ".call.cns")), header=TRUE) %>% dplyr::mutate(p_bintest = NA) %>% dplyr::relocate(p_bintest, .after = depth)
-  callBinFiles <- read.table(file.path(outputFolder, paste0("calls/", s, "bintest.call.cns")), header=TRUE)
-  sam <- rbind(callFiles, callBinFiles)
-
-  #Put sample names and the consequence of the cnv
-  sam2 <- sam %>%
-    dplyr::rowwise() %>%
-    dplyr::filter(cn != 2) %>%
-    dplyr::mutate(Sample= s,
-                  CNV.type = ifelse(cn < 2 ,
+      #Put sample names and the consequence of the cnv
+      sam2 <- sam %>%
+        dplyr::rowwise() %>%
+        dplyr::filter(cn != 2) %>%
+        dplyr::mutate(Sample= s,
+                      CNV.type = ifelse(cn < 2 ,
                                     "deletion",
                                     ifelse(cn>2,
                                            "duplication",
                                             "")))
-  #Merge all in the same file
-  cnvData <- rbind(cnvData, sam2)
-}
+        #Merge all in the same file
+        cnvData <- rbind(cnvData, sam2)
+      }
 
-#Read the bed file and pass cnv results to a GRanges object
-bedGR <- regioneR::toGRanges(bedFile)
-resultsGR <- regioneR::toGRanges(cnvData %>% as.data.frame())
+    #Read the bed file and pass cnv results to a GRanges object
+    bedGR <- regioneR::toGRanges(bedFile)
+    resultsGR <- regioneR::toGRanges(cnvData %>% 
+                                       as.data.frame())
 
-# Create a dataframe to store results with the associated gene
-resGenes <- data.frame()
-for (i in seq_len(length(resultsGR))){
-  #get the CNV
-  cnvGR <- regioneR::toGRanges(resultsGR[i,])
-  # Merge the cnv with the bed file to know the "affected" genes.
-  # Mutate columns to include information related to the cnv of interest (quality, the copy_number, Sample..)
-  #Group by gene to only have one row per gene and get the smallest start coordinate and the biggest end coordinate
-  res <- as.data.frame(IRanges::subsetByOverlaps(bedGR, cnvGR)) %>%
-    dplyr::mutate(CNV.type = cnvGR$CNV.type,
-                  log2 = cnvGR$log2,
-                  #width = cnvGR$width,
-                  cn = cnvGR$cn,
-                  Sample = resultsGR$Sample[i]) %>%
-    dplyr::group_by(name, Sample, seqnames, CNV.type) %>%
-    dplyr::summarise(start_gene = min(start),
-                     end_gene= max (end),
-                     cn = min(cn),
-                     log2 = min(log2))
-  # Merge each cnv result per gene with the rest
-  resGenes <- rbind(resGenes, res)
-}
+    # Create a dataframe to store results with the associated gene
+    resGenes <- data.frame()
+    for (i in seq_len(length(resultsGR))){
+      #get the CNV
+      cnvGR <- regioneR::toGRanges(resultsGR[i,])
+      # Merge the cnv with the bed file to know the "affected" genes.
+      # Mutate columns to include information related to the cnv of interest (quality, the copy_number, Sample..)
+      # Group by gene to only have one row per gene and get the smallest start coordinate and the biggest end coordinate
+      res <- as.data.frame(IRanges::subsetByOverlaps(bedGR, cnvGR)) %>%
+        dplyr::mutate(CNV.type = cnvGR$CNV.type,
+                      log2 = cnvGR$log2,
+                      cn = cnvGR$cn,                  
+                      Sample = resultsGR$Sample[i]) %>%
+        dplyr::group_by(name, Sample, seqnames, CNV.type) %>%
+        dplyr::summarise(start_gene = min(start), 
+                         end_gene = max (end),
+                         cn = min(cn),
+                         log2 = min(log2))
+      # Merge each cnv result per gene with the rest
+      resGenes <- rbind(resGenes, res)
+      }
 
-# Save results----
-# Print results in a tsv file
-finalSummaryFile <- file.path(outputFolder, "cnvs_summary.tsv")
-write.table(resGenes, finalSummaryFile, sep = "\t", quote = F, row.names = F)
+    # Print results in a tsv file
+    finalSummaryFile <- file.path(outputFolder, "cnvs_summary.tsv")
+    write.table(resGenes, finalSummaryFile, sep = "\t", quote = FALSE, row.names = FALSE)
 
-# Save results in a GenomicRanges object
-message("Saving CNV GenomicRanges results")
-saveResultsFileToGR(outputFolder, basename(finalSummaryFile), geneColumn = "name",
-                    sampleColumn = "Sample", chrColumn = "seqnames", startColumn = "start_gene",
-                    endColumn = "end_gene", cnvTypeColumn = "CNV.type")
-
-#Delete temporary files if specified
-if(includeTempFiles == "false"){
-  filesAll <- list.files(outputFolder, full.names = TRUE)
-  filesToKeep <- c("failedROIs.csv", "grPositives.rds", "cnvs_summary.tsv", "cnvFounds.csv", "cnvFounds.txt", "all_cnv_calls.txt", "calls_all.txt", "failures_Failures.txt", "cnv_calls.tsv")
-  filesToRemove <- list(filesAll[!(filesAll %in% grep(paste(filesToKeep, collapse= "|"), filesAll, value=TRUE))])
-  do.call(unlink, filesToRemove)
-}
-
+    ##GenomicRanges object----
+    message("Saving CNV GenomicRanges results")
+    saveResultsFileToGR(outputFolder, basename(finalSummaryFile), geneColumn = "name",
+                        sampleColumn = "Sample", chrColumn = "seqnames", startColumn = "start_gene",
+                        endColumn = "end_gene", cnvTypeColumn = "CNV.type")
+    
+    ##Temporary files----
+    #Delete temporary files if specified
+    if(includeTempFiles == "false"){
+      filesAll <- list.files(outputFolder, full.names = TRUE)
+      filesToKeep <- c("failedROIs.csv", "grPositives.rds", "cnvs_summary.tsv", "cnvFounds.csv", "cnvFounds.txt", "all_cnv_calls.txt", "calls_all.txt", "failures_Failures.txt", "cnv_calls.tsv")
+      filesToRemove <- list(filesAll[!(filesAll %in% grep(paste(filesToKeep, collapse = "|"), filesAll, value=TRUE))])
+      do.call(unlink, filesToRemove)
+    }
   }
 }
+
+print(paste("Finishing at", endTime <- Sys.time()))
+cat("\nElapsed time:")
+print(endTime - startTime)
+
